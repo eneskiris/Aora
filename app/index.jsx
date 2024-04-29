f026948a525ebe0155ebe53dca59e05ed0261d56
enes <eneskiris@gmail.com>
2024-04-30 00:10:01 +0300

    adds bottom tabs

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,14 @@
 import { Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pblack">Hello World</Text>
+      <Link href="/home" className="text-blue-500">
+        Go to Home
+      </Link>
       <StatusBar style="auto" />
     </View>
   );
